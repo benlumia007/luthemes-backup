c2dd650c39c35d8931321801503fe676c9ab603f
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-09-28 17:29:50 -0700

    updated

--- app/Widget/Portfolio/Component.php
+++ app/Widget/Portfolio/Component.php
@@ -14,10 +14,10 @@ class Component extends WP_Widget implements Bootable {
 
     public function form( $instance ) {
  
-        $title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( '', 'text_domain' );
+        $title = ! empty( $instance[ 'title' ] ) ? $instance[ 'title']  : esc_html__( '', 'luthemes' );
         ?>
         <p>
-            <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php echo esc_html__( 'Title:', 'text_domain' ); ?></label>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php echo esc_html__( 'Title:', 'luthemes' ); ?></label>
             <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
         </p>
         <?php
