275306fce9c36e0fbcc57bfd3e39a4e9b3068ae9
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-10-07 13:09:04 -0700

    here, we are going to remove wp-block-styles since this is ClassicPress

--- app/functions-setup.php
+++ app/functions-setup.php
@@ -48,16 +48,13 @@ add_action( 'after_setup_theme', function() {
 		 * By adding add_theme_support( 'html5', arrayy() );, this feature when enabled allows the user use of HTML5 markup for
 		 * comment list, comment forms, search forms, galleries, and captions.
 		 */
-		add_theme_support(
-			'html5',
-			array(
-				'comment-list',
-				'comment-form',
-				'search-form',
-				'gallery',
-				'caption',
-			)
-		);
+		add_theme_support( 'html5', array(
+			'comment-list',
+			'comment-form',
+			'search-form',
+			'gallery',
+			'caption',
+		) );
 
 		/**
 		 * By adding add_theme_support( 'post-thumbnails' );, this feature when enabled allows you to setup featured images
@@ -83,12 +80,7 @@ add_action( 'after_setup_theme', function() {
 		 * By add_image_size( 'initiator-large-thumbnails', 1170, 614, true );. This should be used for content that has no sidebars.
 		 */
 		add_image_size( 'initiator-large-thumbnails', 1170, 614, true );
-
-		/**
-		 * By add_theme_support( 'wp-block-styles' );. This should enable block styles on the frontend.
-		 */
-		add_theme_support( 'wp-block-styles' );
-
+		
 		/**
 		 * By adding add_theme_support( 'editor-styles' ); and add_editor_style(); to enable styles in the backend of the editor.
 		 */
