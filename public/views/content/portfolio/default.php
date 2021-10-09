e898f6ab5f96ba2cb65afc6217e2a1a7e541a453
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-10-08 22:37:50 -0700

    add portfolio/default.php

---
+++ public/views/content/portfolio/default.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Luthemes ( content-single.php )
+ *
+ * @package   Luthemes
+ * @author    Benjamin Lu ( benlumia007@gmail.com )
+ * @copyright Copyright (C) 2014-2021. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/luthemes
+ */
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if ( has_post_thumbnail() ) { ?>
+		<picture class="post-thumbnail">
+			<?php the_post_thumbnail( 'luthemes-medium-thumbnails' ); ?>
+		</picture>
+	<?php } ?>
+	<header class="entry-header">
+		<?php Benlumia007\Backdrop\Theme\Entry\display_title(); ?>
+	</header>
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages(
+				array(
+					'before'      => '<div class="page-links">' . esc_html__( 'Pages:', 'luthemes' ),
+					'after'       => '</div>',
+					'link_before' => '<span>',
+					'link_after'  => '</span>',
+					'pagelink'    => '<span class="screen-reader-text">' . esc_html__( 'Page', 'luthemes' ) . ' </span>%',
+					'separator'   => '<span class="screen-reader-text">,</span> ',
+				)
+			);
+			?>
+	</div>
+	<div class="entry-taxonomies">
+		<?php Benlumia007\Backdrop\Theme\Entry\display_categories(); ?>
+		<?php Benlumia007\Backdrop\Theme\Entry\display_tags(); ?>
+	</div>
+</article>
