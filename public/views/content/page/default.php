04ad3396493f22864e368dca30857200d1a73541
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-10-08 21:41:43 -0700

    updated

--- public/views/content/page/default.php
+++ public/views/content/page/default.php
@@ -8,25 +8,12 @@
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
  * @link      https://github.com/benlumia007/luthemes)
  */
-
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> 
-	<header class="entry-header">
-		<?php Benlumia007\Backdrop\Theme\Entry\display_title(); ?>
-	</header>
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages(
-				array(
-					'before'      => '<div class="page-links">' . esc_html__( 'Pages:', 'luthemes' ),
-					'after'       => '</div>',
-					'link_before' => '<span>',
-					'link_after'  => '</span>',
-					'pagelink'    => '<span class="screen-reader-text">' . esc_html__( 'Page', 'luthemes' ) . ' </span>%',
-					'separator'   => '<span class="screen-reader-text">,</span> ',
-				)
-			);
-			?>
-	</div>
+		<header class="entry-header">
+			<?php Benlumia007\Backdrop\Theme\Entry\display_title(); ?>
+		</header>
+		<div class="entry-content">
+			<?php the_content(); ?>
+		</div>
 </article>
