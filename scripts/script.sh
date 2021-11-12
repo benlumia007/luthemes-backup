e6a35833e3c8b4721dfe220c30dd6697a22932eb
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-11-12 13:38:56 -0800

    remove script

--- scripts/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ ! -d "/home/luthemes/public_html/wp-content/themes/luthemes" ]]; then
-    mv -f "luthemes" "/home/luthemes/public_html/wp-content/themes/luthemes"
-else
-    rsync -r "/home/luthemes/actions-runner/luthemes/luthemes/luthemes/luthemes" "/home/luthemes/public_html/wp-content/themes"
-fi
