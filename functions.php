d125edb8934394f8b9a7ad87df120faad6f0ee64
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-11-12 07:55:21 -0800

    initial commit

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Main function file.
+ *
+ * @package   luthemes
+ * @author    Benjamin Lu ( benlumia007@gmail.com )
+ * @copyright Copyright (C) 2017-2021. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/luthemes
+ */
+
+/**
+ * Table of Content
+ *
+ * 1.0 - Backdrop
+ */
+
+/**
+ * 1.0 - Backdrop
+ */
+if ( file_exists( get_theme_file_path( '/vendor/autoload.php' ) ) ) {
+	require_once get_theme_file_path( '/vendor/autoload.php' );
+}
