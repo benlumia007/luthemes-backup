464858f486064ecb75e121f4d94fbfcb66639913
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-11-12 10:42:04 -0800

    updated

--- scripts/script.sh
+++ scripts/script.sh
@@ -3,5 +3,5 @@
 if [[ ! -d "/home/luthemes/public_html/wp-content/themes/luthemes" ]]; then
     mv -f "luthemes" "/home/luthemes/public_html/wp-content/themes/luthemes"
 else
-    pwd
+    ls -la
 fi
