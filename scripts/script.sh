ee29c702626d23a21e3404841cedbe3596178b89
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-11-12 10:39:51 -0800

    updated

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ! -d "/home/luthemes/public_html/wp-content/themes" ]]; then
+if [[ ! -d "/home/luthemes/public_html/wp-content/themes/luthemes" ]]; then
     mv -f "luthemes" "/home/luthemes/public_html/wp-content/themes/luthemes"
 else
     rsync -r "luthemes/*" "/home/luthemes/public_html/wp-content/themes/luthemes"
