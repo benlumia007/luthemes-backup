b6e410c78f5ebfeeb146d4a52bcc023851dc38d2
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-11-13 23:02:56 -0800

    updated

--- functions.php
+++ functions.php
@@ -4,19 +4,16 @@
  *
  * @package   luthemes
  * @author    Benjamin Lu ( benlumia007@gmail.com )
- * @copyright Copyright (C) 2017-2021. Benjamin Lu
+ * @copyright Copyright (C) 2014-2021. Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
  * @link      https://github.com/benlumia007/luthemes
  */
 
 /**
- * Table of Content
- *
- * 1.0 - Backdrop
- */
-
-/**
- * 1.0 - Backdrop
+ * Run the composer autoloader.
+ * 
+ * Automatically loads the composer autoloader. Be sure to check if the
+ * file exists. This also autoloads our theme's framework, Backdrop.
  */
 if ( file_exists( get_theme_file_path( '/vendor/autoload.php' ) ) ) {
 	require_once get_theme_file_path( '/vendor/autoload.php' );
