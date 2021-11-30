91f4de1421bd9dcdd02ab8d235dc187e0b5a0320
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-11-30 13:13:35 -0800

    updated

---
+++ app/functions-filters.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Theme - filters
+ * 
+ * This file enqueue scripts for css and js.
+ *
+ * @package   luthemes
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright Copyright (C) 2017-2021. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/luthemes
+ */
+
+namespace Benlumia007\Luthemes;
