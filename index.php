87408a7510d364313e63d6bdab127d458139202d
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-11-30 15:38:34 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Luthemes ( index.php )
+ *
+ * @package   Luthemes
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright Copyright (C) 2014-2021. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/**
+ * Defines the root directory
+ * 
+ * Automatically defines LUTHEMES_DIR. 
+ */
+define( 'LUTHEMES_DIR', __DIR__ );
+
+/**
+ * Run the composer autoloader.
+ * 
+ * Automatically loads the composer autoloader.
+ */
+require_once( 'vendor/autoload.php' );
